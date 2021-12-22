ff4bd5be2eb9ea270611a8b3506685fe9ec2d6a2
Jonas Jordan <jj@sernet.de>
2021-12-22 12:42:06 +0100

    fix latest form template bundle query
    VEO-1073

---
+++ src/main/kotlin/org/veo/forms/migrations/V5__fixSemVerCollation.kt
@@ -0,0 +1,37 @@
+/**
+ * verinice.veo forms
+ * Copyright (C) 2021  Jonas Jordan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.veo.forms.migrations
+
+import org.flywaydb.core.api.migration.BaseJavaMigration
+import org.flywaydb.core.api.migration.Context
+
+class V5__fixSemVerCollation : BaseJavaMigration() {
+    override fun migrate(context: Context) {
+        context.connection.createStatement().use {
+            it.execute(
+                """
+                create collation sem_ver (locale = 'en@colNumeric=yes', provider = 'icu');
+
+                alter table form_template_bundle
+                    alter column version type varchar(255) collate sem_ver;
+ 
+                """
+            )
+        }
+    }
+}
---
+++ src/test/kotlin/org/veo/forms/FormTemplateBundleJpaTest.kt
@@ -0,0 +1,55 @@
+
+/**
+ * verinice.veo forms
+ * Copyright (C) 2021  Jonas Jordan
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.veo.forms
+
+import io.kotest.matchers.shouldBe
+import net.swiftzer.semver.SemVer
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.veo.forms.mvc.AbstractSpringTest
+import java.util.UUID.randomUUID
+
+class FormTemplateBundleJpaTest : AbstractSpringTest() {
+
+    @Autowired
+    private lateinit var repo: FormTemplateBundleJpaRepository
+
+    @Test
+    fun findsLatestBundle() {
+        // Given various form template bundle versions and one for a deviating domain template
+        val domainTemplateId = randomUUID()
+        repo.saveAll(
+            listOf(
+                FormTemplateBundle(domainTemplateId, SemVer(1, 2, 0), emptyMap()),
+                FormTemplateBundle(domainTemplateId, SemVer(1, 2, 3), emptyMap()),
+                FormTemplateBundle(domainTemplateId, SemVer(1, 13, 0), emptyMap()),
+                FormTemplateBundle(domainTemplateId, SemVer(1, 13, 1), emptyMap()),
+                FormTemplateBundle(domainTemplateId, SemVer(0, 45, 5), emptyMap()),
+                FormTemplateBundle(randomUUID(), SemVer(2, 60, 7), emptyMap()),
+            )
+        )
+
+        // when querying the latest bundle
+        val latest = repo.getLatest(domainTemplateId)!!
+
+        // then the bundle with the correct domain template and highest version number is returned
+        latest.domainTemplateId shouldBe domainTemplateId
+        latest.version shouldBe SemVer(1, 13, 1)
+    }
+}
